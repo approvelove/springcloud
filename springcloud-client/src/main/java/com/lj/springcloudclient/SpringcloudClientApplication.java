d87708bee6c89e29e8033019b60187f9a19a47c8
李健 <1205247242@qq.com>
2019-04-03 17:31:25 +0800

    短路器
    新增Hystrix断路器监控面板

--- springcloud-client/src/main/java/com/lj/springcloudclient/SpringcloudClientApplication.java
+++ springcloud-client/src/main/java/com/lj/springcloudclient/SpringcloudClientApplication.java
@@ -1,17 +1,31 @@
 package com.lj.springcloudclient;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class SpringcloudClientApplication {
+
+	/**
+	 * 访问地址 http://localhost:8762/actuator/hystrix.stream
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudClientApplication.class, args);
@@ -21,7 +35,12 @@ public class SpringcloudClientApplication {
 	String port;
 
 	@RequestMapping("hi")
+	@HystrixCommand(fallbackMethod = "hiError")
 	public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
 		return "hi " + name + " ,i am from port:" + port;
+	}
+
+	public String hiError(String name) {
+		return "hi," + name + ", sorry, error!";
 	}
 }
